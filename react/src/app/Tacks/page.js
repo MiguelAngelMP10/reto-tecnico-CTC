4681552e141deab4a420aa39e625254b5bc9b0c7
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-09-28 12:46:43 -0600

    •  Filtrado y Búsqueda: Los usuarios deben tener la capacidad de buscar tareas por título o estado en la interfaz

--- react/src/app/Tacks/page.js
+++ react/src/app/Tacks/page.js
@@ -6,15 +6,21 @@ import 'react-toastify/dist/ReactToastify.css';
 import Link from "next/link";
 import Menu from "@/app/components/Menu";
 
+
 export default function Index() {
 
     let [data, setData] = useState({});
     const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState('');
+    const [selectedValue, setSelectedValue] = useState('');
+    const [states, setStates] = useState({})
+
+
     useEffect(() => {
         async function fetchData() {
             try {
                 setLoading(true);
-                const response = await fetch('http://127.0.0.1:8000/api/tasks');
+                const response = await fetch(`http://127.0.0.1:8000/api/tasks`);
                 const data = await response.json();
                 setTimeout(() => {
                     setLoading(false);
@@ -25,7 +31,18 @@ export default function Index() {
             }
         }
 
+        async function getStates() {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/api/states');
+                const data = await response.json();
+                setStates(data);
+            } catch (error) {
+                console.error('Error al obtener los datos:', error);
+            }
+        }
+
         fetchData().then();
+        getStates().then();
     }, []);
 
     function handleClick(id) {
@@ -104,7 +121,31 @@ export default function Index() {
         }
     }
 
+    const handleInputChange = (e) => {
+        setSearch(e.target.value);
+    };
+    const searchByFilter = async () => {
+        let filter = getFilter(search, selectedValue);
 
+        try {
+            setLoading(true);
+            const response = await fetch(`http://127.0.0.1:8000/api/tasks${filter}`);
+            const data = await response.json();
+            setTimeout(() => {
+                setLoading(false);
+            }, 2000);
+            setData(data);
+        } catch (error) {
+            console.error('Error al obtener los datos:', error);
+        }
+    }
+
+    const getFilter = (title, state) => {
+        return `?filter[title]=${title}&filter[state]=${state}`;
+    }
+    const handleSelectChange = (e) => {
+        setSelectedValue(e.target.value);
+    };
     return (
         <>
             <Menu></Menu>
@@ -145,6 +186,64 @@ export default function Index() {
                             </th>
                             <th scope="col" className="px-6 py-3 text-center" colSpan={3}>
                                 Acciones
+                            </th>
+                        </tr>
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                <label htmlFor="default-search"
+                                       className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                <div className="relative">
+                                    <div
+                                        className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                                  strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                        </svg>
+                                    </div>
+                                    <input type="search" id="default-search"
+                                           className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           placeholder="Search by title"
+                                           value={search}
+                                           onChange={handleInputChange}
+                                    />
+                                    <button type="button" onClick={searchByFilter}
+                                            className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <i className="fa-solid fa-magnifying-glass"></i> Search
+                                    </button>
+                                </div>
+
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+
+                            </th>
+                            <th scope="col" className="min-w-max">
+                                <select value={selectedValue} onChange={handleSelectChange}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option value="">Select an option</option>
+                                    {states.data?.map((row) => (
+                                        <option key={row.id} value={row.id}>
+                                            {row.abbreviation} {row.name}
+                                        </option>
+                                    ))}
+                                </select>
+
+                                <button type="button" onClick={searchByFilter}
+                                        className="text-white w-full mt-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                    <i className="fa-solid fa-magnifying-glass"></i> Search
+                                </button>
+
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-center" colSpan={3}>
+
                             </th>
                         </tr>
                         </thead>
