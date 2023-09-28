afa6571528cee4729d77fb67e8beff564ebcee3a
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2023-09-27 20:07:47 -0600

    add route user index

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('tasks', \App\Http\Controllers\TaskController::class);
+Route::resource('users', \App\Http\Controllers\UserController::class)->only('index');
